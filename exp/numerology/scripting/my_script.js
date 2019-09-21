cc4b8ac7e1fb722c755ad2381bcdc6f12e2419fc
Tricia L Holman <32218171+Stormy9@users.noreply.github.com>
2019-09-21 13:29:56 -0700

    added switch statement for different life-path #'s
    and it worked!  ha!
    go back and fill in actual responses next.

--- exp/numerology/scripting/my_script.js
+++ exp/numerology/scripting/my_script.js
@@ -60,17 +60,75 @@ function life_path() {
     // bring in the traits-by-number + build list:
     document.getElementById("list_header").innerHTML = "key traits of a " + sum + " life path:"
 
-    var li_01 = $('<li></li>').text('sweet'); 
-    
-    var li_02 = $('<li></li>').text('funny');
-
-  	var li_03 = $('<li></li>').text('loveable');
-
-    var li_04 = $('<li></li>').text('really sharp');
-
-    var li_05 = $('<li></li>').text('but a little obtuse at times')
-
-        $('#list_spot').append(li_01, li_02, li_03, li_04, li_05); 
+    var li_01, li_02, li_03, li_04, li_05;
+    switch(sum) {
+        case 1:
+            li_01 = $('<li></li>').text('sweet'); 
+            li_02 = $('<li></li>').text('funny');
+  	        li_03 = $('<li></li>').text('loveable');
+            li_04 = $('<li></li>').text('super sharp');
+            li_05 = $('<li></li>').text('occasionally obtuse');
+            break;
+        case 2:
+            li_01 = $('<li></li>').text('two'); 
+            li_02 = $('<li></li>').text('two');
+  	        li_03 = $('<li></li>').text('two');
+            li_04 = $('<li></li>').text('two');
+            li_05 = $('<li></li>').text('two');
+            break;
+        case 3:
+            li_01 = $('<li></li>').text('three'); 
+            li_02 = $('<li></li>').text('three');
+  	        li_03 = $('<li></li>').text('three');
+            li_04 = $('<li></li>').text('three');
+            li_05 = $('<li></li>').text('three');
+            break;
+        case 4:
+            li_01 = $('<li></li>').text('four'); 
+            li_02 = $('<li></li>').text('four');
+  	        li_03 = $('<li></li>').text('four');
+            li_04 = $('<li></li>').text('four');
+            li_05 = $('<li></li>').text('four');
+            break;
+        case 5:
+            li_01 = $('<li></li>').text('five'); 
+            li_02 = $('<li></li>').text('five');
+  	        li_03 = $('<li></li>').text('five');
+            li_04 = $('<li></li>').text('five');
+            li_05 = $('<li></li>').text('five');
+            break;
+        case 6:
+            li_01 = $('<li></li>').text('six'); 
+            li_02 = $('<li></li>').text('six');
+  	        li_03 = $('<li></li>').text('six');
+            li_04 = $('<li></li>').text('six');
+            li_05 = $('<li></li>').text('six');
+            break;
+        case 7:
+            li_01 = $('<li></li>').text('seven'); 
+            li_02 = $('<li></li>').text('seven');
+  	        li_03 = $('<li></li>').text('seven');
+            li_04 = $('<li></li>').text('seven');
+            li_05 = $('<li></li>').text('seven');
+            break;
+        case 8:
+            li_01 = $('<li></li>').text('eight'); 
+            li_02 = $('<li></li>').text('eight');
+  	        li_03 = $('<li></li>').text('eight');
+            li_04 = $('<li></li>').text('eight');
+            li_05 = $('<li></li>').text('eight');
+            break;
+        case 9:
+            li_01 = $('<li></li>').text('nine'); 
+            li_02 = $('<li></li>').text('nine');
+  	        li_03 = $('<li></li>').text('nine');
+            li_04 = $('<li></li>').text('nine');
+            li_05 = $('<li></li>').text('nine');
+            break;
+        default:
+            li_01 = $('<li></li>').text('apparently there was some mistake somewhere');
+    }
+    $('#list_spot').append(li_01, li_02, li_03, li_04, li_05); 
 }
 // end life_path function
 //========================================================
@@ -79,22 +137,24 @@ function add_up(total, value) {
   return total + value;
 }
 //=======================================================
+// reset function:
 document.getElementById("reset").addEventListener("click", reset)
 
 function reset() {
-    // in order to clear out previous input in text-box,
+    // in order to clear out previous input from the text-box,
     // the text-box & buttons have to be inside of
     // a <form></form> element
 
     // plus we wanna do this, too:
     $('#list_header, #list_spot').text('');
     $('#life_path_number, #list_header, #list_spot').hide();
-    
-    // got this from my 'GuessingGame' code:
+
+    // trying this from my 'GuessingGame' code:
     location.reload(true);
-    // b/c after clicking 'reset', it worked fine IF
+    // b/c after clicking 'reset', things worked fine IF
     // you did everything correctly..... 
-    // BUT -- it wouldn't do it's validation thing
-    // if you didn't.....
-        // just like g_oz & GuessingGame
+    // BUT -- it wouldn't do it's validation thing (once
+    // 'reset' was clicked) if you didn't.....
+        // just like with 'g_oz' & 'GuessingGame'
+    // there's gotta be a better way than this though?
 }
