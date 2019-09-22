2eb3a845567ae94b06d5151edb62ef6deea8457e
Tricia L Holman <32218171+Stormy9@users.noreply.github.com>
2019-09-21 17:03:04 -0700

    added some actual <li> responses   [=

--- exp/numerology/scripting/my_script.js
+++ exp/numerology/scripting/my_script.js
@@ -1,12 +1,13 @@
 // we don't want this showing right away.....
-// and to separately control what it *does* say, when it *does* show!
+// and to separately control what it *does* say,
+// when it *does* show!
 $('#result').hide();
 
 document.getElementById("click_me").addEventListener("click", life_path);
 
 function life_path() {
-    // clear out previous results -- otherwise they just stack up!
-    // as in, the <ul> just keeps adding <li>'s
+    // clear out previous results:
+    // otherwise they just stack up!
     $('#list_header, #list_spot').text('');
     // if you include #list_holder, nothing *ever* shows up!
 
@@ -15,6 +16,7 @@ function life_path() {
 
     if(isNaN(input) || array.length != 8) {
         var oops = 'oops! enter your b-day like this:  01031999';
+        // put this in #result not #life_path_number
         $('#result').show();
         document.getElementById('result').innerHTML = oops;
         $('#result').css({"color": "teal", "font-weight": "bold"});
@@ -46,22 +48,22 @@ function life_path() {
         }
         //------------------------------------------------
         //------------------------------------------------
-        // > it's *gotta* be b/c of the <p> & <span> thing...
-        // > yeah that actually makes some sense.....
-        // > got it to function like i want with 'hide()' and 'show()'
+        // it's *gotta* be b/c of the <p> & <span> thing...
+        // yeah that actually makes some sense.....
+        // got it to function like i want with 'hide()' and 'show()'
         // 
         // um, NO ^^^^^ you DIDN'T.....
         // it worked *ONLY* if everything was input correctly 
-        // EVERY time -- if the input ever generated the 'oops'
+        // EVERY time -- if the input ever generated the oops
         // message, and then you fixed it to be correct input,
-        // IT ALL BROKE!!  as in, nothing happened and you got
-        // that same error message of:
+        // IT ALL BROKE!!  Nothing happened and you got that
+        // same error message of:
         //  'TypeError: Cannot set property 'innerHTML' of null'
         //
         // and of *COURSE* it's the <p> & <span> thing!!!
         // you were WIPING OUT the html for the <span>, by
         // setting the innerHTML for #result here!
-        // and not putting in the stupid <span> tags -- with 
+        // and not putting the stupid <span> tags -- with 
         // the #life_path_number id -- INTO that innerHTML!!!
         //
         // geezus you can be obtuse at times.
@@ -78,23 +80,23 @@ function life_path() {
     }
 
     // bring in the traits-by-number + build list:
-    document.getElementById("list_header").innerHTML = "key traits of a " + sum + " life path:"
+    document.getElementById("list_header").innerHTML = "key traits of a '" + sum + "' life path:"
 
-    var li_01, li_02, li_03, li_04, li_05;
+    var li_01, li_02, li_03, li_04, li_05, li_06;
     switch(sum) {
         case 1:
             li_01 = $('<li></li>').text('sweet'); 
-            li_02 = $('<li></li>').text('funny');
-  	        li_03 = $('<li></li>').text('loveable');
-            li_04 = $('<li></li>').text('super sharp');
-            li_05 = $('<li></li>').text('occasionally obtuse');
+            li_02 = $('<li></li>').text('funny + easily amused');
+  	        li_03 = $('<li></li>').text('loving & loveable');
+            li_04 = $('<li></li>').text('super sharp, but...');
+            li_05 = $('<li></li>').text('...occasionally obtuse');
             break;
         case 2:
-            li_01 = $('<li></li>').text('two'); 
-            li_02 = $('<li></li>').text('two');
-  	        li_03 = $('<li></li>').text('two');
-            li_04 = $('<li></li>').text('two');
-            li_05 = $('<li></li>').text('two');
+            li_01 = $('<li></li>').text('a big teddy-bear'); 
+            li_02 = $('<li></li>').text('loving & loveable');
+  	        li_03 = $('<li></li>').text('peaceful soul + a peacemaker');
+            li_04 = $('<li></li>').text('fun, silly sense of humor');
+            li_05 = $('<li></li>').text('dedicated to the happiness of their loved-ones');
             break;
         case 3:
             li_01 = $('<li></li>').text('three'); 
@@ -104,11 +106,12 @@ function life_path() {
             li_05 = $('<li></li>').text('three');
             break;
         case 4:
-            li_01 = $('<li></li>').text('four'); 
-            li_02 = $('<li></li>').text('four');
-  	        li_03 = $('<li></li>').text('four');
-            li_04 = $('<li></li>').text('four');
-            li_05 = $('<li></li>').text('four');
+            li_01 = $('<li></li>').text('loving & loveable'); 
+            li_02 = $('<li></li>').text("a 'rescuer' with a huge heart");
+            li_03 = $('<li></li>').text('keen & off-beat sense of humor');
+  	        li_04 = $('<li></li>').text('fun & engaging storyteller');
+            li_05 = $('<li></li>').text('flourishes in serenity');
+            li_06 = $('<li></li>').text('brings sunshine into the lives of everyone lucky enough to be around them');
             break;
         case 5:
             li_01 = $('<li></li>').text('five'); 
@@ -148,7 +151,7 @@ function life_path() {
         default:
             li_01 = $('<li></li>').text('apparently there was some mistake somewhere');
     }
-    $('#list_spot').append(li_01, li_02, li_03, li_04, li_05); 
+    $('#list_spot').append(li_01, li_02, li_03, li_04, li_05, li_06); 
 }
 // end life_path function
 //========================================================
